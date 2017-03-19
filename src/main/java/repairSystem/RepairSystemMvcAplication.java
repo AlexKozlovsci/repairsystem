c08536173a7b190ffbfbfb1f771db942a052fbf5
Gleb Legchilov <betrazen13@gmail.com>
2017-03-19 12:20:57 +0300

    Create java project

---
+++ src/main/java/repairSystem/RepairSystemMvcAplication.java
@@ -0,0 +1,15 @@
+package repairSystem;
+
+/**
+ * Created by Глеб on 19.03.2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairSystemMvcAplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RepairSystemMvcAplication.class, args);
+    }
+}
