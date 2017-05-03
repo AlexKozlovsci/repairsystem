61fa665f383eac6c30c0ae6a47f99bfe76af36c6
Sych Yury <sych_y@mail.ru>
2017-05-04 01:08:40 +0300

    Add some details logic

--- src/main/java/repairSystem/controller/AdminController.java
+++ src/main/java/repairSystem/controller/AdminController.java
@@ -1,9 +1,17 @@
 package repairSystem.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import repairSystem.dao.DetailRepository;
+import repairSystem.model.Detail;
+
+import java.util.List;
 
 /**
  * Created by Глеб on 01.05.2017.
@@ -19,10 +27,41 @@ public class AdminController {
         return mav;
     }
 
+    private static final Logger log = Logger.getLogger(AdminController.class);
+    @Autowired
+    private DetailRepository detailRepository;
+
     @RequestMapping(value = "/admin/parts/", method = RequestMethod.GET)
-    public ModelAndView parts(){
+    public ModelAndView parts(Model model){
+        ModelAndView mav = new ModelAndView();
+        List<Detail> det = (List<Detail>) detailRepository.findAll();
+        mav.addObject("details", det);
+        mav.setViewName("admin/parts");
+        return mav;
+    }
+
+    @RequestMapping(value = "/admin/parts/", method = RequestMethod.POST)
+    public ModelAndView parts(@ModelAttribute Detail detail, Model model) {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("admin/parts");
+        return mav;
+    }
+
+    @RequestMapping(value = "/admin/editParts/", method = RequestMethod.GET)
+    public ModelAndView editParts(@ModelAttribute Detail detail, Detail d) {
+        ModelAndView mav = new ModelAndView();
+        Detail det = (Detail) detailRepository.findById(d.getId());
+        mav.addObject("detail", det);
+        log.info(det);
+        log.info(d.getId());
+        mav.setViewName("admin/editParts");
+        return mav;
+    }
+
+    @RequestMapping(value = "/admin/editParts/", method = RequestMethod.POST)
+    public ModelAndView editParts(Model model) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("admin/editParts");
         return mav;
     }
 
---
+++ src/main/java/repairSystem/dao/DetailRepository.java
@@ -0,0 +1,17 @@
+package repairSystem.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import repairSystem.model.Detail;
+
+import java.util.List;
+
+/**
+ * Created by Юрий on 03.05.2017.
+ */
+
+@Repository
+public interface DetailRepository extends JpaRepository<Detail, Long> {
+    List<Detail> findAll();
+    Detail findById(long id);
+}
