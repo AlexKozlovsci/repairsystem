4b8fbda3995c4ee624bcf524032cd7500c13e5ad
Sych Yury <sych_y@mail.ru>
2017-05-21 15:34:55 +0300

    Add engineer order list

--- src/main/java/repairSystem/controller/EngineerController.java
+++ src/main/java/repairSystem/controller/EngineerController.java
@@ -1,9 +1,24 @@
 package repairSystem.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import repairSystem.dao.ClientRepository;
+import repairSystem.dao.UserRepository;
+import repairSystem.dao.WorkorderRepository;
+import repairSystem.model.Workorder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Vlad on 01.05.2017.
@@ -12,16 +27,51 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class EngineerController {
 
+    private static final Logger log = Logger.getLogger(EngineerController.class);
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private WorkorderRepository workorderRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+
     @RequestMapping(value = "/engineer/", method = RequestMethod.GET)
     public ModelAndView index(){
         ModelAndView mav = new ModelAndView();
+        User authUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        repairSystem.model.User u = userRepository.findByLogin(authUser.getUsername());
+        Map<String, String> mapManagers = new HashMap<String, String>();
+        Workorder item;
+        repairSystem.model.User manager;
+        List<Workorder> workorders = (List<Workorder>) workorderRepository.findAllByIdEngineer(u.getId());
+        for (Iterator<Workorder> i = workorders.iterator(); i.hasNext();) {
+            item = i.next();
+            Integer temp = (int)item.getId();
+            String id = temp.toString();
+            manager = userRepository.findById(item.getId_manager());
+            String managerName = manager.getName().concat(" ").concat(manager.getSecondname());
+            mapManagers.put(id, managerName);
+            log.info(id);
+            log.info(managerName);
+        }
+        mav.addObject("orders", workorders);
+        mav.addObject("managers", mapManagers);
         mav.setViewName("engineer/index");
         return mav;
     }
 
-    @RequestMapping(value = "/engineer/order", method = RequestMethod.GET)
-    public ModelAndView order(){
+    @RequestMapping(value = "/engineer/order", method = RequestMethod.GET, params = {"id"})
+    public ModelAndView order(@ModelAttribute Workorder workorder, final HttpServletRequest req){
+        final Integer orderId = Integer.valueOf(req.getParameter("id"));
+        workorder = (Workorder) workorderRepository.findById(orderId);
+        repairSystem.model.User manager = userRepository.findById(workorder.getId_manager());
+        String managerName = manager.getName().concat(" ").concat(manager.getSecondname());
+        repairSystem.model.Client client = clientRepository.findById(workorder.getId_manager());
+        String clientName = client.getName().concat(" ").concat(client.getSecondname());
         ModelAndView mav = new ModelAndView();
+        mav.addObject("order", workorder);
+        mav.addObject("manager", managerName);
+        mav.addObject("client", clientName);
         mav.setViewName("engineer/order");
         return mav;
     }
--- src/main/java/repairSystem/dao/WorkorderRepository.java
+++ src/main/java/repairSystem/dao/WorkorderRepository.java
@@ -1,6 +1,8 @@
 package repairSystem.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import repairSystem.model.Workorder;
 
@@ -14,5 +16,8 @@ import java.util.List;
 public interface WorkorderRepository extends JpaRepository<Workorder, Long>{
     List<Workorder> findAll();
     Workorder findById(long id);
+
+    @Query(value = "SELECT * FROM workorder WHERE id_engineer = :id", nativeQuery = true)
+    List<Workorder> findAllByIdEngineer(@Param("id") long id);
     Workorder save(Workorder workorder);
 }
